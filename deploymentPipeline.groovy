10d19af5b2dbef556b5e8b9309edcd0b74a77298
Petar Peshev <petar.peshev@leanplum.com>
2019-07-23 18:04:55 +0300

    test with temp credentials

--- deploymentPipeline.groovy
+++ deploymentPipeline.groovy
@@ -31,8 +31,8 @@ pipeline {
           dir("target") {
             echo "Installing plugin ...."
             withCredentials([usernameColonPassword(credentialsId: 'lpadmin', variable: 'USERPASS')]) { 
-              //def CRUMB = sh (script: """curl -s -k 'https://$USERPASS@${env.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'""",returnStdout: true)
-              sh """curl -s -k 'https://$USERPASS@${env.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'"""
+             // def CRUMB = sh (script: """curl -s 'https://$USERPASS@${env.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'""",returnStdout: true)
+              sh """curl -s 'https://$USERPASS@${env.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'"""
 //            sh """curl -X POST -H "$CRUMB" --user $USERPASS -i -F file=@ghprb.hpi http://${env.JENKINS_URL}/pluginManager/uploadPlugin"""
           }
         }
