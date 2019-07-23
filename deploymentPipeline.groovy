caa586287f2efa7e31db6936784761aceeee0d56
Petar Peshev <petar.peshev@leanplum.com>
2019-07-23 15:31:42 +0300

    test the ssl skip for curl

--- deploymentPipeline.groovy
+++ deploymentPipeline.groovy
@@ -17,13 +17,13 @@ pipeline {
 
   stages {
     
-  stage("Build ghprb plugin from leanplum fork repository...") {
+   /*stage("Build ghprb plugin from leanplum fork repository...") {
       steps {
         script {
             sh 'mvn package' 
         }
       }
-    }
+    }*/
 
     stage ("Install the plugin to Jenkins...") {
       steps {
@@ -31,9 +31,8 @@ pipeline {
           dir("target") {
             echo "Installing plugin ...."
             withCredentials([usernameColonPassword(credentialsId: 'lpadmin', variable: 'USERPASS')]) { 
-            def CRUMB = sh (script: """curl -s 'https://$USERPASS@${env.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'""",returnStdout: true)
-
-           // sh """curl -X POST -H "$CRUMB" --user $USERPASS -i -F file=@ghprb.hpi http://${env.JENKINS_URL}/pluginManager/uploadPlugin"""
+            def CRUMB = sh (script: """curl -s -k 'https://$USERPASS@${env.JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'""",returnStdout: true)
+            sh """curl -X POST -H "$CRUMB" --user $USERPASS -i -F file=@ghprb.hpi http://${env.JENKINS_URL}/pluginManager/uploadPlugin"""
           }
         }
        }
